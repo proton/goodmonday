6c7b36f905a097cee4b59183b9866ef5f7d545df
Peter Savichev (proton) <psavichev@gmail.com>
2012-02-16 21:36:02 +0400

    added default (index) page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Cpa::Application.routes.draw do
+	root :to => 'site#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -45,12 +46,6 @@ Cpa::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
