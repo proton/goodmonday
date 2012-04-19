b9dfa3b988f4a64f3212dc8b9ab28c50ef991920
Peter Savichev (proton) <psavichev@gmail.com>
2012-04-19 11:32:06 +0400

    added states(ranks) to webmasters

--- app/models/user/webmaster.rb
+++ app/models/user/webmaster.rb
@@ -1,6 +1,10 @@
 class Webmaster < User
+	include Mongoid::Symbolize
+
 	has_many :grounds
 	has_many :achievements
 
 	field :balance, type: Integer, default: 0
+
+	symbolize :rank, :in => [:bronze, :silver, :gold], :default => :bronze
 end
