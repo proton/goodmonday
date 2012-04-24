c6f9748caaf829d89ef0aa2e9bd148430d17680a
Peter Savichev (proton) <psavichev@gmail.com>
2012-04-24 20:56:04 +0400

    added :admin user_grounds resourse

---
+++ app/controllers/admin/grounds_controller.rb
@@ -0,0 +1,62 @@
+# coding: utf-8
+
+class Admin::GroundsController < Admin::BaseController
+	before_filter :find_nested_objects
+	before_filter :and_nested_crumbs
+	before_filter :find_object, :only => [:show, :update, :edit, :destroy]
+	before_filter :and_crumbs
+
+	def index
+		@grounds = @user.grounds
+	end
+
+	def show
+		add_crumb "Рекламная площадка «#{@ground.title}»"
+	end
+
+	def edit
+		add_crumb "Рекламная площадка «#{@ground.title}»"
+	end
+
+	def new
+		@ground = @user.grounds.new
+		add_crumb "Новая рекламная площадка"
+	end
+
+	def create
+		@ground = @user.grounds.new(params[:ground])
+		flash[:notice] = 'Площадка добавлена.' if @ground.save
+		respond_with(@ground, :location => user_ground_path(@user, @ground))
+	end
+
+	def update
+		@ground = Ground.find(params[:id])
+		flash[:notice] = 'Площадка обновлена.' if @ground.update_attributes(params[:ground])
+		respond_with(@ground, :location => user_ground_path(@user, @ground))
+	end
+
+	def destroy
+		flash[:notice] = 'Площадка удалена.' if @ground.destroy
+		respond_with(@ground, :location => user_grounds_path(@user))
+	end
+
+	protected
+
+	def find_nested_objects
+		@user = User.find(params[:user_id])
+	end
+
+	def and_nested_crumbs
+		add_crumb "Пользователи", users_path
+		add_crumb "Пользователь «#{@user.email}»", user_path(@user)
+	end
+
+	def find_object
+		@ground = @user.grounds.find(params[:id])
+	end
+
+	def and_crumbs
+		add_crumb "Рекламные площадки", user_grounds_path(@user)
+	end
+
+end
