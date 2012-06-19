584a1d83f5f9dcdcc9efd3296eaf9c4ef8221747
Peter Savichev (proton) <psavichev@gmail.com>
2012-06-19 22:28:10 +0400

    now targets have only one webmaster price (only for rank :bronze)

--- app/models/target.rb
+++ app/models/target.rb
@@ -24,6 +24,7 @@ class Target
 
 	MODERATED_ATTRS = %w[title fixed_price prc_price hold]
   MODERATED_ATTRS_INFO = {'fixed_price' => {:type => :currency} }
-  MODERATED_EDIT_FIELDS = [:fixed_prices_bronze, :fixed_prices_silver, :fixed_prices_gold, :prc_prices_bronze, :prc_prices_silver, :prc_prices_gold]
+  #MODERATED_EDIT_FIELDS = [:fixed_prices_bronze, :fixed_prices_silver, :fixed_prices_gold, :prc_prices_bronze, :prc_prices_silver, :prc_prices_gold]
+  MODERATED_EDIT_FIELDS = [:fixed_prices_bronze, :prc_prices_bronze]
 	include IsModerated
 end
