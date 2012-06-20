0a5d7cc24f1552a0d2b41b58dd439391b4ae00b7
Peter Savichev (proton) <psavichev@gmail.com>
2012-06-20 12:05:36 +0400

    moving carrierwave uploads to ext.goodmonday.ru

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => 'FUdRmY7pgdUw89m3QuUku8GkJEUZ7HWRW25jDsvW',
     #:region                 => 'reg-eu-ireland'
   }
-  config.fog_directory  = 'goodmonday'
-  #config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
+  config.fog_directory  = 'ext.goodmonday'
+  config.fog_host       = 'http://ext.goodmonday.ru'
   #config.fog_public     = false                                   # optional, defaults to true
   #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
