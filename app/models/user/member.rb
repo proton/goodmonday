f356b19179556a54f1a4aae5ba3890ed42c7bfa5
Peter Savichev (proton) <psavichev@gmail.com>
2012-06-22 10:16:45 +0400

    updatede /my ui (added referal stats and some small improvements)

--- app/models/user/member.rb
+++ app/models/user/member.rb
@@ -8,6 +8,11 @@ class Member < User
   field :total_payments, type: Integer, default: 0
   embeds_many :payments #, cascade_callbacks: true
 
+  #referal system
+  field :referal_count, type: Integer, default: 0
+  field :referal_hold_balance, type: Integer, default: 0
+  field :referal_total_payments, type: Integer, default: 0
+
   def can_pay? sum
     (self.balance+self.overdraft) > sum
   end
