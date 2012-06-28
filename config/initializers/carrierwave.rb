44dcd106874b52f1a0fdc074346f742af39dca48
Peter Savichev (proton) <psavichev@gmail.com>
2012-06-28 14:18:19 +0400

    fixed uploading images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => 'AKIAISUPHX6DDALNHMBQ',
     :aws_secret_access_key  => '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',
-    #:region                 => 'reg-eu-ireland'
+    :region                 => 'eu-west-1'
   }
   config.fog_directory  = 'ext.goodmonday.ru'
   config.fog_host       = 'http://ext.goodmonday.ru'
