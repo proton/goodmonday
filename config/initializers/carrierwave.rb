90e149a4d64c810624b58f6b728a3dcd62346b0e
Peter Savichev (proton) <psavichev@gmail.com>
2012-06-29 15:40:10 +0400

    added different carrierwave settings for production and other environments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,18 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAISUPHX6DDALNHMBQ',
-    :aws_secret_access_key  => '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',
-    :region                 => 'eu-west-1'
-  }
-  config.fog_directory  = 'ext.goodmonday.ru'
-  config.fog_host       = 'http://ext.goodmonday.ru'
-  #config.fog_public     = false                                   # optional, defaults to true
-  #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => 'AKIAISUPHX6DDALNHMBQ',
+      :aws_secret_access_key  => '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',
+      :region                 => 'eu-west-1'
+    }
+    config.fog_directory  = 'ext.goodmonday.ru'
+    config.fog_host       = 'http://ext.goodmonday.ru'
+    #config.fog_public    = false                                   # optional, defaults to true
+    config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}  # optional, defaults to {}
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
 end
