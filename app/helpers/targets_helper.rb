82a13cd0e97a106b703e3d7fa95ba551e982e9d3
Peter Savichev (proton) <psavichev@gmail.com>
2012-07-03 15:49:54 +0400

    fixed bug in show_target_price method

--- app/helpers/targets_helper.rb
+++ app/helpers/targets_helper.rb
@@ -1,9 +1,11 @@
 module TargetsHelper
 	def show_target_price(prc_price, fixed_price)
+    fixed_price_exists = (fixed_price && fixed_price>0)
+    prc_price_exists = (prc_price && prc_price>0)
     s = ''
-    s+="#{prc_price} %" if prc_price && prc_price>0
-    s+=" + " if prc_price && prc_price>0 && fixed_price>0
-    s+=number_to_rubles(fixed_price) if fixed_price && fixed_price>0
+    s+="#{prc_price} %" if prc_price_exists
+    s+=" + " if prc_price_exists && fixed_price_exists
+    s+=number_to_rubles(fixed_price) if fixed_price_exists
     s
   end
 end
