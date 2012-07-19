9fd90706130cbea5f10d32664d4698739fbaf340
Peter Savichev (proton) <psavichev@gmail.com>
2012-07-19 14:20:37 +0400

    fixed target_form (changed decimal inputs to float)

--- app/helpers/targets_helper.rb
+++ app/helpers/targets_helper.rb
@@ -1,9 +1,9 @@
 module TargetsHelper
 	def show_target_price(prc_price, fixed_price)
     fixed_price_exists = (fixed_price && fixed_price>0)
-    prc_price_exists = (prc_price && prc_price>0)
+    prc_price_exists = (prc_price && prc_price.to_f>0)
     s = ''
-    s+="#{prc_price} %" if prc_price_exists
+    s+="#{prc_price.to_f} %" if prc_price_exists
     s+=" + " if prc_price_exists && fixed_price_exists
     s+=number_to_currency(fixed_price) if fixed_price_exists
     s
