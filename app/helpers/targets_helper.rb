a6672c4f789579cbe8b477fd6b17c4016a357dd2
Peter Savichev (proton) <psavichev@gmail.com>
2012-07-19 14:23:06 +0400

    small ui fixes

--- app/helpers/targets_helper.rb
+++ app/helpers/targets_helper.rb
@@ -3,7 +3,7 @@ module TargetsHelper
     fixed_price_exists = (fixed_price && fixed_price>0)
     prc_price_exists = (prc_price && prc_price.to_f>0)
     s = ''
-    s+="#{prc_price.to_f} %" if prc_price_exists
+    s+="#{prc_price} %" if prc_price_exists
     s+=" + " if prc_price_exists && fixed_price_exists
     s+=number_to_currency(fixed_price) if fixed_price_exists
     s
