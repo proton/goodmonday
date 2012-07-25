b664d9f8717b25067b47d2cd6b050c448a9cab51
Peter Savichev (proton) <psavichev@gmail.com>
2012-07-25 19:44:17 +0400

    fixed offers sorting on /offers and home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def index
     @news = News.order_by(:created_at)
     # @offers = Offer.accepted.where(is_adult: false).order_by([:epc, :desc]).limit(3)
-    @offers = Offer.accepted.order_by([:epc, :desc]).limit(3)
+    @offers = Offer.accepted.order_by([:epc_cents, :desc]).limit(3)
   end
 
   protected
