600f9cf676b9e131e85d770de7db3631163878ff
Peter Savichev (proton) <psavichev@gmail.com>
2012-08-01 13:42:40 +0400

    fixed timestamps in payments

--- app/models/user/payment.rb
+++ app/models/user/payment.rb
@@ -1,10 +1,11 @@
 class Payment
   include Mongoid::Document
- 	include Mongoid::Timestamps
+ 	include Mongoid::Timestamps::Created
   include Mongoid::MoneyField
   embedded_in :member
 
   before_create :increase_member_balance
+  before_create :set_timestamp
 
   money_field :amount
   field :description, type: String, default: ''
@@ -14,5 +15,9 @@ class Payment
   def increase_member_balance
     self.member.balance += self.amount
     self.member.save
+  end
+
+  def set_timestamp
+    self.created_at = Time.now
   end
 end
