242982a0bc605c69f19639451416cf00809a7fc8
Peter Savichev (proton) <psavichev@gmail.com>
2012-08-13 11:48:31 +0400

    small ui change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,8 @@ class HomeController < ApplicationController
   def index
     @news = News.order_by(:created_at)
     # @offers = Offer.accepted.where(is_adult: false).order_by([:epc, :desc]).limit(3)
-    @offers = Offer.accepted.order_by([:epc_cents, :desc]).limit(3)
+    #@offers = Offer.accepted.order_by([:epc_cents, :desc]).limit(3)
+    @offers = Offer.accepted.where('targets.moderated_state' => :accepted).order_by([:epc_cents, :desc]).limit(3)
   end
 
   protected
