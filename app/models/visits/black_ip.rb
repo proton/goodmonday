794a4ac2ce27006bba9f4e5a4b3f2ea5d157a293
Peter Savichev (proton) <psavichev@gmail.com>
2012-08-17 14:03:12 +0400

    added list of free proxies

--- app/models/visits/black_ip.rb
+++ app/models/visits/black_ip.rb
@@ -3,11 +3,12 @@ class BlackIp
 	include Mongoid::Timestamps
 	include Mongoid::Symbolize
 
-	symbolize :type, :in => [:permanently, :temporarily], :default => :permanently, :scopes => true
+  symbolize :type, :in => [:permanently, :temporarily], :default => :permanently, :scopes => true
 
 	field :ip, type: String
 	index :ip, unique: true
 	field :discard_at, type: DateTime
+  field :reason, type: String
 
 	def self.include(ip)
 		BlackIp.where(:ip => ip).size!=0
