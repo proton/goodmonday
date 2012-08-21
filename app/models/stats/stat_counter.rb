e328c61f06731996e0dd02808852e15d1190352b
Peter Savichev (proton) <psavichev@gmail.com>
2012-08-21 14:30:47 +0400

    added new stats model

---
+++ app/models/stats/stat_counter.rb
@@ -0,0 +1,31 @@
+class StatCounter
+	include Mongoid::Document
+
+  belongs_to :ground, index: true
+  belongs_to :offer, index: true
+  belongs_to :advertiser, index: true
+  belongs_to :webmaster, index: true
+
+  field :date, type: Date
+  field :sub_id, type: String
+  field :target_id, type: BSON::ObjectId
+
+  field :clicks, type: Integer, default: 0
+  field :targets, type: Integer, default: 0
+  field :income, type: Integer, default: 0 #доход вебмастера, в копейках (cents), ибо проще считать
+  field :expenditure, type: Integer, default: 0 #расход рекламодателя, в копейках (cents), ибо проще считать
+
+  index :date
+  index :sub_id
+
+  index({ webmaster_id: 1, date: 1 })
+  index({ adversiter_id: 1, date: 1 })
+
+  def self.register_click(ground, offer, sub_id)
+    StatCounter.find_or_create_by(ground_id: ground.id, offer_id: offer.id, advertiser_id: offer.advertiser.id, webmaster_id: ground.webmaster.id, date: Date.today, sub_id: sub_id).inc(:clicks, 1)
+    StatCounter.find_or_create_by(ground_id: ground.id, offer_id: offer.id, advertiser_id: offer.advertiser.id, webmaster_id: ground.webmaster.id, date: Date.new(0), sub_id: sub_id).inc(:clicks, 1)
+  end
+
+  def self.inst()
+  end
+end
