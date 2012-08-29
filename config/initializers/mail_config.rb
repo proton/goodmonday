42f9a9c721a2eba20174bd9b24880a78f180547b
Peter Savichev (proton) <psavichev@gmail.com>
2012-08-30 00:37:04 +0400

    fixed mail sending

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings = {  
-	:address              => "smtp.yandex.ru",  
-	:port                 => 587,  
-	:domain               => "goodmonay.ru",
-	:user_name            => "robot@goodmonay.ru",
-	:password             => "uPg5kZ6J7D",
-	:authentication       => :plain,  
-	:enable_starttls_auto => false,
-	:tsl                  => true
+  :address              => "smtp.yandex.ru",
+  :port                 => 587,
+  :domain               => "goodmonday.ru",
+  :user_name            => "robot@goodmonday.ru",
+  :password             => "uPg5kZ6J7D",
+  :authentication       => :plain,
+  :enable_starttls_auto => false,
+  :tsl                  => true
 }
