8e845f8087868ce18d9110945d20a1478ebe6d04
Peter Savichev (proton) <psavichev@gmail.com>
2012-09-11 18:27:23 +0400

    changed sekret token to avoid problem with old signed cookies

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cpa::Application.config.secret_token = 'e61318720df9d7634c2f2a4d5d41656415ad02f2f6b10463c81fb6eb91f85fd6b4cd53b2e8bec8f81854e817883129ac05963ea7799afa83c96a21216f1286af'
+Cpa::Application.config.secret_token = 'e61318720cf9d7634c2f2a4d5d41656415ad02f2f6b10463c81fb6eb91f85fd6b4cd53b2e8bec8f81854e817883129ac05963ea7799afa83c96a21216f1286af'
