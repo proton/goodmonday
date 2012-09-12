fe1591d1de6550055d34b667a41ed85d791a4818
Peter Savichev (proton) <psavichev@gmail.com>
2012-09-12 14:49:27 +0400

    temporary hack for fix deptors module

--- app/controllers/admin/debtors_controller.rb
+++ app/controllers/admin/debtors_controller.rb
@@ -3,7 +3,8 @@
 class Admin::DebtorsController < Admin::BaseController
 
 	def index
-    @users = User.collection.find({'$where' => 'this.hold_balance_cents < this.overdraft_cents', '_type' => 'Advertiser'})
+    user_ids = User.collection.find({'$where' => 'this.hold_balance_cents < this.overdraft_cents', '_type' => 'Advertiser'}).collect{|e| e['_id']} #TODO: it temporary hack for mongoid3 (collection.find now returns array of Moped::BSON::Document (in 2 version it returns array of Models))
+    @users = User.find(user_ids)
 		add_crumb "Должники"
 	end
 end
