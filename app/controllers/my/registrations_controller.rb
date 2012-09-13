5b69aed43f5bcfd1d49b6c2fae0f14e9028298af
Peter Savichev (proton) <psavichev@gmail.com>
2012-09-13 14:38:34 +0400

    fixed registations module (rewritten devise create method)

--- app/controllers/my/registrations_controller.rb
+++ app/controllers/my/registrations_controller.rb
@@ -14,6 +14,29 @@ class My::RegistrationsController < Devise::RegistrationsController
 		end
   end
 
+  def create
+    #copied from https://github.com/plataformatec/devise/blob/master/app/controllers/devise/registrations_controller.rb
+    user_params = params[:user]
+    user_type = user_params[:_type]
+    user_type = 'Webmaster' unless ['Webmaster', 'Advertiser'].include? user_type
+    self.resource = user_type.constantize.new_with_session(user_params, session)
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
   def after_sign_up_path_for(resource)
     root_url(:subdomain => 'my')
   end
