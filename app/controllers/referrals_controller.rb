17bbeac07366cbbcebe71b325a2df2635d8de406
Peter Savichev (proton) <psavichev@gmail.com>
2012-09-25 23:23:35 +0400

    fixed goodmonday referral system

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -2,6 +2,15 @@
 
 class ReferralsController < ApplicationController
 
+  def index
+    if params[:referral] && !params[:referral].empty?
+      referral_id = params[:referral]
+      referral = User.where(:_id => referral_id).first
+      cookies[:referral] = referral_id if referral
+    end
+    redirect_to root_url
+  end
+
 	def show
     if params[:id] && !params[:id].empty?
       referral_id = params[:id]
